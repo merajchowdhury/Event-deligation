111e1e2c709dbbd96a1c60171f469f5f141ef651
merajchowdhury <megherchaya71@gmail.com>
2023-12-29 12:45:39 +0600

    selected

--- java.js
+++ java.js
@@ -7,6 +7,12 @@
         change.style.color = 'white';
     }
 })
+/*
+ami kintu nicher code golo uporo functin er modde rakthe cheyeche but kaj korteche na, abar sports ke ekta vaiable er modde rakthe cheyeche tao kaj korteche na, shudo niche rakle kaj kore
+
+color gulo vinno korte cheyeche toa kar kortheche na. 
+link FreeCodeCamp.org
+*/ 
 let sports = document.querySelector('#sports');
 let li = document.createElement('li');
 li.innerText = 'Badminton';
